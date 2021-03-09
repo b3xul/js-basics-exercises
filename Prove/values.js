a317fb788e29b7d83121ac829fc375119560880e
b3xul <bexul97@gmail.com>
2021-03-09 09:13:49 +0100

    my values.js exercise

---
+++ Prove/values.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let a = "abc";
+
+console.log(a);
+
+/*
+    run via terminal: node values.js
+    run via interpreter (output in debug console): run menu->start debugging (F5) or run without debuggin(CTRL+F5)-> choose execution engine (Chrome/Node.js)
+    
+    Lateral run menu-> create launch configuration for Node.js/Chrome -> default configuration
+    Config must be in the same folder as the file
+    */
+
+a = 5;
+
+console.log(a);
