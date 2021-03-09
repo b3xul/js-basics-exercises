6dce9235c0adedb3dac3a0b4228799a0d9045114
b3xul <bexul97@gmail.com>
2021-03-09 11:36:29 +0100

    Update info

---
+++ Prove/basic_environment.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let a = "abc";
+
+console.log(a);
+
+/*
+    run via terminal: node values.js
+    run via interpreter (output in debug console): run menu->start debugging (F5) or run without debuggin(CTRL+F5)-> choose execution engine (Chrome/Node.js)
+    N.B. Interpreter includes debugging functionalities!!! Terminal can only show output!
+    
+    Lateral run menu-> create launch configuration for Node.js/Chrome -> default configuration
+    Config must be in the same folder as the file
+    */
+
+a = 5;
+
+console.log(a);
