3dc7f34deb0e0a09cd83698001d0d29b9bb4cbba
b3xul <bexul97@gmail.com>
2021-03-31 19:19:17 +0200

    rename and organize notes

--- Snippets/basic_environment.js
+++ Snippets/basic_environment.js
@@ -5,7 +5,7 @@ let a = "abc";
 console.log(a);
 
 /*
-    run via terminal: node values.js
+    run via terminal: node basic_environment.js
     run via interpreter (output in debug console): run menu->start debugging (F5) or run without debuggin(CTRL+F5)-> choose execution engine (Chrome/Node.js)
     N.B. Interpreter includes debugging functionalities!!! Terminal can only show output!
     
